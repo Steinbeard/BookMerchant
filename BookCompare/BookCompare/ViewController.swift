a449d0b099bca0062077ff961aa4f14ad373c973
Daniel Steinberg <danielmaxsteinberg@gmail.com>
2020-03-23 01:12:14 -0700

    Scanning a barcode succesfully fetches from OpenLibrary API and builds correct book model. Next populate detail page with info, and grab price info (real or placeholder). Yay

---
+++ BookCompare/BookCompare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookCompare
+//
+//  Created by Daniel Steinberg on 3/7/20.
+//  Copyright © 2020 Daniel Steinberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Readme.md
@@ -0,0 +1,5 @@
+An app for comparing online book prices and availability
+
+Use Rainforest API (trial) to retreive prices from Amazon.com with UPC
+Or something else??
+ISBN -> Book model by GETing from http://openlibrary.org/api/books?jscmd=data&bibkeys=ISBN
