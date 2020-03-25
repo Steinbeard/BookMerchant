89e17d26a4395a432020c93c28a4f76b5aa667a0
Daniel Steinberg <danielmaxsteinberg@gmail.com>
2020-03-25 14:00:03 -0700

    Building book add form

---
+++ BookCompare/BookCompare/NewListingForm/NewListingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewListingTableViewCell.swift
+//  BookCompare
+//
+//  Created by Daniel Steinberg on 3/25/20.
+//  Copyright © 2020 Daniel Steinberg. All rights reserved.
+//
+
+import UIKit
+
+class NewListingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
